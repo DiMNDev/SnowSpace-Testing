865798e8ef35ced259880211c2f436bb2359dbd1
Joshua Arnold <joshua.arnold@students.snow.edu>
2024-09-25 09:21:11 -0600

    added public methods, private fields, unit tests

--- SnowSpace.TestLib/UnitTest1.cs
+++ SnowSpace.TestLib/UnitTest1.cs
@@ -9,10 +9,52 @@ public class RocketTesting
     public void WhenRocketTakesOffItDoesNotBlowUp()
     {
         //Arrange
-var rocket = new RocketShip("DragonOne");
+        var rocket = new RocketShip("DragonOne");
         //Act
-    var result = rocket.Takeoff();
+        var result = rocket.Takeoff();
         //Assert
         result.Should().NotBe("BLOWED UP!");
+    }
+
+    [Fact]
+    public void CanChangeName()
+    {
+        // Given
+        var rocket = new RocketShip("Original1");
+        // When
+        rocket.SetName("Changed1");
+        // Then
+        rocket.GetName().Should().Be("Changed1");
+    }
+
+    [Fact]
+    public void CannotMakeNullName()
+    {
+        // Given
+        void makeRocketWithNullName()
+        {
+            var rocket = new RocketShip(null);
+        }
+        // When
+        FluentActions
+        .Invoking(makeRocketWithNullName)
+        .Should()
+        .Throw<Exception>();
+        // Then
+
+    }
+
+
+    [Fact]
+    public void RocektWithNoNumberThrowsException()
+    {
+        // Given
+        void makeRocketWithoutNumber()
+        {
+            var rocket = new RocketShip("NoNumberInName");
+        }
+        // When
+        FluentActions.Invoking(makeRocketWithoutNumber).Should().Throw<Exception>();
+        // Then
     }
 }
